a61799728617b589f70f46ecd61d4eb090facb6a
SlavicDeveloper <mde1533@mail.ru>
2021-12-30 17:52:51 +0300

    lesson_16. QuerySet update

--- authnapp/migrations/0007_auto_20211225_2115.py
+++ authnapp/migrations/0007_auto_20211225_2115.py
@@ -9,13 +9,16 @@ from django.utils.timezone import utc
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authnapp', '0006_auto_20211222_1259'),
+        ("authnapp", "0006_auto_20211222_1259"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='shopuser',
-            name='activation_key_expires',
-            field=models.DateTimeField(default=datetime.datetime(2021, 12, 27, 21, 15, 51, 156009, tzinfo=utc), verbose_name='актуальность ключа'),
+            model_name="shopuser",
+            name="activation_key_expires",
+            field=models.DateTimeField(
+                default=datetime.datetime(2021, 12, 27, 21, 15, 51, 156009, tzinfo=utc),
+                verbose_name="актуальность ключа",
+            ),
         ),
     ]
